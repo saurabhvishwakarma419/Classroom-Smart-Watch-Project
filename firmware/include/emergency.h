bb042e14dae5584d46997e9851a15a751e5eb4d1
Saurabh vishwakarma <saurvishwakarma040207@gmail.com>
2025-11-21 08:44:41 +0530

    Update emergency.h

--- firmware/include/emergency.h
+++ firmware/include/emergency.h
@@ -1,1 +1,36 @@
+#ifndef EMERGENCY_H
+#define EMERGENCY_H
 
+#include <Arduino.h>
+
+// Emergency alert structure
+struct EmergencyAlert {
+    String studentId;
+    String alertType;
+    float latitude;
+    float longitude;
+    String location;
+    unsigned long timestamp;
+    String deviceMac;
+};
+
+// Emergency states
+enum EmergencyState {
+    NORMAL,
+    ALERT_TRIGGERED,
+    ALERT_SENT,
+    ALERT_ACKNOWLEDGED
+};
+
+// Function declarations
+void initEmergency();
+void checkEmergencyButton();
+bool triggerEmergencyAlert();
+bool sendEmergencyToServer(const EmergencyAlert &alert);
+void displayEmergencyScreen();
+EmergencyState getEmergencyState();
+void resetEmergencyState();
+bool isEmergencyButtonPressed();
+void vibrate(int duration);
+
+#endif
